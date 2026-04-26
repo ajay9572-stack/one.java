e09952c1c2bb48a98157d6ab6a249c84b0f44e32
kumar <kumarajay912282@gmail.com>
2026-04-26 14:17:03 +0530

    Fix output formatting in class One for clarity

--- One.java
+++ One.java
@@ -3,10 +3,10 @@ public class One {
     public static void main(String[] args) {
         int a = 10,b= 20;
         if(a>b){
-            System.out.println("a is greater");
+            System.out.println("a is      greater");
         }
         else{
-            System.out.println("b is greater");
+            System.out.println("b is greater........");
         }
     }
 }
