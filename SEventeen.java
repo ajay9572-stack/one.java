ed4915f183f76703efa1daf2cd021379fd19a08b
kumar <kumarajay912282@gmail.com>
2026-05-02 14:14:42 +0530

    Fix class name typo from SEventeen to Seventeen

--- SEventeen.java
+++ SEventeen.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
-public class SEventeen {
+public class Seventeen {
+    
         // reverse a number
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
