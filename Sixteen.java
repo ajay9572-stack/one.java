7cbc943d2b7bb7f4aa2d64768ece4f1863474e9a
kumar <kumarajay912282@gmail.com>
2026-05-02 13:31:04 +0530

    Add class Sixteen to calculate factorial using recursion

---
+++ Sixteen.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class FactorialByRecursion {
+    static int fact = 1;
+
+    public static void main(String args[]) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter the no: ");
+        int no = s.nextInt();
+
+        FactorialByRecursion ob = new FactorialByRecursion();
+        ob.calcFact(no);
+
+        System.out.println("factorial of " + no + " is " + fact);
+    }
+
+    void calcFact(int no) {
+        if (no >= 1) {
+            fact = fact * no;
+            calcFact(no - 1);
+        }
+    }
+}
