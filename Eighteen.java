5fa7d9f60b10d4429bb6ba93c37f3aa7949e6cf9
kumar <kumarajay912282@gmail.com>
2026-05-03 11:58:07 +0530

    Add missing scanner close statement in Eighteen class

--- Eighteen.java
+++ Eighteen.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class Eighteen {
+    // reverse a string
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a string: ");
@@ -12,5 +13,6 @@ public class Eighteen {
         }
 
         System.out.println("Reversed string: " + reversed);
+        sc.close();
     }
 }
