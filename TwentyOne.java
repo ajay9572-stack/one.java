61d4254fb845862e8193b7c629dad9e3a30167b2
kumar <kumarajay912282@gmail.com>
2026-05-04 12:13:18 +0530

    Add class TwentyOne to calculate the sum of numbers from 1 to n

---
+++ TwentyOne.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class TwentyOne {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        
+        int i = 1, sum = 0;
+        
+        while(i <= n) {
+            sum += i;
+            i++;
+        }
+        
+        System.out.println("Sum = " + sum);
+    }
+}
