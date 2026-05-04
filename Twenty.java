b2226977ae3b30cd50b845f8ca72fb1897a00912
kumar <kumarajay912282@gmail.com>
2026-05-04 12:11:08 +0530

    Add comment to clarify the purpose of the Twenty class

--- Twenty.java
+++ Twenty.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class Twenty {
+    // print first n natural numbers
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
