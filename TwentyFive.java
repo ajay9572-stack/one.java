f2a7d17479376efa883247ab55d8183dcbe4e82d
kumar <kumarajay912282@gmail.com>
2026-05-06 11:42:22 +0530

    Add class TwentyFive to print first n Fibonacci numbers

---
+++ TwentyFive.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+// print first n Fibonacci numbers
+
+public class TwentyFive {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");   
+        int n = sc.nextInt();
+        int a =0, b= 1;
+        int c;
+        for(int i = 1; i <= n; i++) {
+            System.out.print(a + " ");
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        sc.close();
+    }
+}
