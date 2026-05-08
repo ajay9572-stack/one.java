8d7f87f93550d8950a6df858c59434fe37aba046
kumar <kumarajay912282@gmail.com>
2026-05-08 20:06:41 +0530

    Add class ThirtyFour to print an inverted right-angled triangle of asterisks based on user input

--- ThirtyFour.java
+++ ThirtyFour.java
@@ -1,24 +1,27 @@
 import java.util.Scanner;
 
-public class ThirtyThree {
+public class ThirtyFour {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter n: ");  
+
+        System.out.print("Enter n: ");
         int n = sc.nextInt();
 
         for(int i = 1; i <= n; i++) {
 
-            for(int j = n-1; j >= i; j--)
-           {
+            // spaces
+            for(int j = 1; j < i; j++) {
                 System.out.print("  ");
             }
-            for( int k = 1; k <= i; k++)
-           {
+
+            // stars
+            for(int k = n; k >= i; k--) {
                 System.out.print("* ");
-            }   
+            }
+
             System.out.println();
-          
         }
+
         sc.close();
     }
 }
