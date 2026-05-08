d8efc30a7ce6bd14b91c3897843dc4b69282b7f2
kumar <kumarajay912282@gmail.com>
2026-05-08 19:51:36 +0530

    Add class ThirtyThree to print a left-aligned triangle of asterisks based on user input

---
+++ ThirtyThree.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ThirtyThree {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n: ");  
+        int n = sc.nextInt();
+
+        for(int i = 1; i <= n; i++) {
+
+            for(int j = n-1; j >= i; j--)
+           {
+                System.out.print("  ");
+            }
+            for( int k = 1; k <= i; k++)
+           {
+                System.out.print("* ");
+            }   
+            System.out.println();
+          
+        }
+    }
+}
