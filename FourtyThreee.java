8194d34cbd8803f7b8d8fcd29e3471fbb2c8464c
kumar <kumarajay912282@gmail.com>
2026-05-10 10:22:11 +0530

    Add class FourtyThreee to print a hollow pyramid shape of asterisks based on user input

---
+++ FourtyThreee.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class FourtyThreee {
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
+
+            for (int k = 1; k <2*i; k++) {
+                if(k==1 || k==2*i-1){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+    }
+    }
+}
