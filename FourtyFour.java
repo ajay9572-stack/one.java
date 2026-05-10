75d781b7cb679d7d96117d9055ce67d310c7a89f
kumar <kumarajay912282@gmail.com>
2026-05-10 10:27:45 +0530

    Add class FourtyFour to print an inverted hollow pyramid shape of asterisks based on user input

---
+++ FourtyFour.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class FourtyFour {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+
+        for (int i = n; i >= 1; i--) {
+            for (int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k < 2 * i; k++) {
+
+                if (k == 1 || k == 2 * i - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
